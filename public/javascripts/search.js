21cf034f79dadeb52c50fb116f00b00b5ff51a13
lentmiien <lentmiien@gmail.com>
2020-05-24 00:40:02 +0900

    layouting

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -103,130 +103,103 @@ function ChangeSearch() {
           dhl: Available(output[i].dhl_available, output[i].dhl_averagetime, output[i].dhl_totalaveragetime),
           airp: Available(output[i].airp_available, output[i].airp_averagetime, output[i].airp_totalaveragetime),
         };
-        document.getElementById('output').innerHTML = `
-                <h3>${output[i].country_name} (${output[i].country_code})</h3>
-                ${tracking_number.length > 0 ? '<p>' + tracking_number + '</p>' : ''}
-                <table class="table table-dark table-striped">
-                    <thead>
-                        <tr>
-                            <th span="col"></th>
-                            <th span="col" title="Available, delay and Suspended can be selected by customers.\nFor Suspended, the order will be shipped when the shipping method becomes available again.">Status*</th>
-                            <th span="col">Average last 2 weeks<br>(compared to all time average)</th>
-                            <th span="col" title="Shipped and delivered">Last successfully<br>shipped*</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th span="row">EMS</th>
-                            <td><span class="${format.ems.class}" title="${format.ems.title}">${format.ems.text}</span></td>
-                            <td>${
-                              output[i].ems_averagetime > 0 ? Math.round(10 * output[i].ems_averagetime) / 10 : '--'
-                            } days <span class="${
+        document.getElementById('country').innerText = `${output[i].country_name} (${output[i].country_code})`;
+        document.getElementById('tracking').innerText = tracking_number;
+
+        // EMS
+        document.getElementById(
+          'ems_available'
+        ).innerHTML = `<span class="${format.ems.class}" title="${format.ems.title}">${format.ems.text}</span>`;
+        document.getElementById('ems_average').innerHTML = `${
+          output[i].ems_averagetime > 0 ? Math.round(10 * output[i].ems_averagetime) / 10 : '--'
+        } days <span class="${
           Math.round(10 * output[i].ems_averagetime) / 10 - output[i].ems_totalaveragetime < 0 ? 'fast' : 'slow'
         }" title="Overall average shipping time: ${output[i].ems_totalaveragetime} days">(${
           output[i].ems_averagetime > 0 ? Math.round(10 * (output[i].ems_averagetime - output[i].ems_totalaveragetime)) / 10 : '--'
-        } days)</span></td>
-                            <td>${
-                              output[i].ems_lastsucessfullyshipped > 0 ? new Date(output[i].ems_lastsucessfullyshipped).toDateString() : ''
-                            }</td>
-                        </tr>
-                        <tr>
-                            <th span="row">ASP</th>
-                            <td><span class="${format.airsp.class}" title="${format.airsp.title}">${format.airsp.text}</span></td>
-                            <td>${
-                              output[i].airsp_averagetime > 0 ? Math.round(10 * output[i].airsp_averagetime) / 10 : '--'
-                            } days <span class="${
+        } days)</span>`;
+        document.getElementById('ems_lastshipped').innerHTML = `${
+          output[i].ems_lastsucessfullyshipped > 0 ? new Date(output[i].ems_lastsucessfullyshipped).toDateString() : ''
+        }`;
+
+        // ASP
+        document.getElementById(
+          'airsp_available'
+        ).innerHTML = `<span class="${format.airsp.class}" title="${format.airsp.title}">${format.airsp.text}</span>`;
+        document.getElementById('airsp_average').innerHTML = `${
+          output[i].airsp_averagetime > 0 ? Math.round(10 * output[i].airsp_averagetime) / 10 : '--'
+        } days <span class="${
           Math.round(10 * output[i].airsp_averagetime) / 10 - output[i].airsp_totalaveragetime < 0 ? 'fast' : 'slow'
         }" title="Overall average shipping time: ${output[i].airsp_totalaveragetime} days">(${
           output[i].airsp_averagetime > 0 ? Math.round(10 * (output[i].airsp_averagetime - output[i].airsp_totalaveragetime)) / 10 : '--'
-        } days)</span></td>
-                            <td>${
-                              output[i].airsp_lastsucessfullyshipped > 0
-                                ? new Date(output[i].airsp_lastsucessfullyshipped).toDateString()
-                                : ''
-                            }</td>
-                        </tr>
-                        <tr>
-                            <th span="row">SAL Reg.</th>
-                            <td><span class="${format.salspr.class}" title="${format.salspr.title}">${format.salspr.text}</span></td>
-                            <td>${
-                              output[i].salspr_averagetime > 0 ? Math.round(10 * output[i].salspr_averagetime) / 10 : '--'
-                            } days <span class="${
+        } days)</span>`;
+        document.getElementById('airsp_lastshipped').innerHTML = `${
+          output[i].airsp_lastsucessfullyshipped > 0 ? new Date(output[i].airsp_lastsucessfullyshipped).toDateString() : ''
+        }`;
+
+        // SAL Reg.
+        document.getElementById(
+          'salspr_available'
+        ).innerHTML = `<span class="${format.salspr.class}" title="${format.salspr.title}">${format.salspr.text}</span>`;
+        document.getElementById('salspr_average').innerHTML = `${
+          output[i].salspr_averagetime > 0 ? Math.round(10 * output[i].salspr_averagetime) / 10 : '--'
+        } days <span class="${
           Math.round(10 * output[i].salspr_averagetime) / 10 - output[i].salspr_totalaveragetime < 0 ? 'fast' : 'slow'
         }" title="Overall average shipping time: ${output[i].salspr_totalaveragetime} days">(${
           output[i].salspr_averagetime > 0 ? Math.round(10 * (output[i].salspr_averagetime - output[i].salspr_totalaveragetime)) / 10 : '--'
-        } days)</span></td>
-                            <td>${
-                              output[i].salspr_lastsucessfullyshipped > 0
-                                ? new Date(output[i].salspr_lastsucessfullyshipped).toDateString()
-                                : ''
-                            }</td>
-                        </tr>
-                        <tr>
-                            <th span="row">SAL Unreg.</th>
-                            <td><span class="${format.salspu.class}" title="${format.salspu.title}">${format.salspu.text}</span></td>
-                            <td>${
-                              output[i].salspu_averagetime > 0 ? Math.round(10 * output[i].salspu_averagetime) / 10 : '--'
-                            } days <span class="${
-          Math.round(10 * output[i].salspu_averagetime) / 10 - output[i].salspu_totalaveragetime < 0 ? 'fast' : 'slow'
-        }" title="Overall average shipping time: ${output[i].salspu_totalaveragetime} days">(${
-          output[i].salspu_averagetime > 0 ? Math.round(10 * (output[i].salspu_averagetime - output[i].salspu_totalaveragetime)) / 10 : '--'
-        } days)</span></td>
-                            <td>${
-                              output[i].salspu_lastsucessfullyshipped > 0
-                                ? new Date(output[i].salspu_lastsucessfullyshipped).toDateString()
-                                : ''
-                            }</td>
-                        </tr>
-                        <tr>
-                            <th span="row">SAL Parcel</th>
-                            <td><span class="${format.salp.class}" title="${format.salp.title}">${format.salp.text}</span></td>
-                            <td>${
-                              output[i].salp_averagetime > 0 ? Math.round(10 * output[i].salp_averagetime) / 10 : '--'
-                            } days <span class="${
+        } days)</span>`;
+        document.getElementById('salspr_lastshipped').innerHTML = `${
+          output[i].salspr_lastsucessfullyshipped > 0 ? new Date(output[i].salspr_lastsucessfullyshipped).toDateString() : ''
+        }`;
+
+        // SAL Unreg.
+        document.getElementById(
+          'salspu_available'
+        ).innerHTML = `<span class="${format.salspu.class}" title="${format.salspu.title}">${format.salspu.text}</span>`;
+
+        // SAL Parcel
+        document.getElementById(
+          'salp_available'
+        ).innerHTML = `<span class="${format.salp.class}" title="${format.salp.title}">${format.salp.text}</span>`;
+        document.getElementById('salp_average').innerHTML = `${
+          output[i].salp_averagetime > 0 ? Math.round(10 * output[i].salp_averagetime) / 10 : '--'
+        } days <span class="${
           Math.round(10 * output[i].salp_averagetime) / 10 - output[i].salp_totalaveragetime < 0 ? 'fast' : 'slow'
         }" title="Overall average shipping time: ${output[i].salp_totalaveragetime} days">(${
           output[i].salp_averagetime > 0 ? Math.round(10 * (output[i].salp_averagetime - output[i].salp_totalaveragetime)) / 10 : '--'
-        } days)</span></td>
-                            <td>${
-                              output[i].salp_lastsucessfullyshipped > 0
-                                ? new Date(output[i].salp_lastsucessfullyshipped).toDateString()
-                                : ''
-                            }</td>
-                        </tr>
-                        <tr>
-                            <th span="row">DHL</th>
-                            <td><span class="${format.dhl.class}" title="${format.dhl.title}">${format.dhl.text}</span></td>
-                            <td>${
-                              output[i].dhl_averagetime > 0 ? Math.round(10 * output[i].dhl_averagetime) / 10 : '--'
-                            } days <span class="${
+        } days)</span>`;
+        document.getElementById('salp_lastshipped').innerHTML = `${
+          output[i].salp_lastsucessfullyshipped > 0 ? new Date(output[i].salp_lastsucessfullyshipped).toDateString() : ''
+        }`;
+
+        // DHL
+        document.getElementById(
+          'dhl_available'
+        ).innerHTML = `<span class="${format.dhl.class}" title="${format.dhl.title}">${format.dhl.text}</span>`;
+        document.getElementById('dhl_average').innerHTML = `${
+          output[i].dhl_averagetime > 0 ? Math.round(10 * output[i].dhl_averagetime) / 10 : '--'
+        } days <span class="${
           Math.round(10 * output[i].dhl_averagetime) / 10 - output[i].dhl_totalaveragetime < 0 ? 'fast' : 'slow'
         }" title="Overall average shipping time: ${output[i].dhl_totalaveragetime} days">(${
           output[i].dhl_averagetime > 0 ? Math.round(10 * (output[i].dhl_averagetime - output[i].dhl_totalaveragetime)) / 10 : '--'
-        } days)</span></td>
-                            <td>${
-                              output[i].dhl_lastsucessfullyshipped > 0 ? new Date(output[i].dhl_lastsucessfullyshipped).toDateString() : ''
-                            }</td>
-                        </tr>
-                        <tr>
-                            <th span="row">Air Parcel</th>
-                            <td><span class="${format.airp.class}" title="${format.airp.title}">${format.airp.text}</span></td>
-                            <td>${
-                              output[i].airp_averagetime > 0 ? Math.round(10 * output[i].airp_averagetime) / 10 : '--'
-                            } days <span class="${
+        } days)</span>`;
+        document.getElementById('dhl_lastshipped').innerHTML = `${
+          output[i].dhl_lastsucessfullyshipped > 0 ? new Date(output[i].dhl_lastsucessfullyshipped).toDateString() : ''
+        }`;
+
+        // Air Parcel
+        document.getElementById(
+          'airp_available'
+        ).innerHTML = `<span class="${format.airp.class}" title="${format.airp.title}">${format.airp.text}</span>`;
+        document.getElementById('airp_average').innerHTML = `${
+          output[i].airp_averagetime > 0 ? Math.round(10 * output[i].airp_averagetime) / 10 : '--'
+        } days <span class="${
           Math.round(10 * output[i].airp_averagetime) / 10 - output[i].airp_totalaveragetime < 0 ? 'fast' : 'slow'
         }" title="Overall average shipping time: ${output[i].airp_totalaveragetime} days">(${
           output[i].airp_averagetime > 0 ? Math.round(10 * (output[i].airp_averagetime - output[i].airp_totalaveragetime)) / 10 : '--'
-        } days)</span></td>
-                            <td>${
-                              output[i].airp_lastsucessfullyshipped > 0
-                                ? new Date(output[i].airp_lastsucessfullyshipped).toDateString()
-                                : ''
-                            }</td>
-                        </tr>
-                    </tbody>
-                </table>
-                `;
+        } days)</span>`;
+        document.getElementById('airp_lastshipped').innerHTML = `${
+          output[i].airp_lastsucessfullyshipped > 0 ? new Date(output[i].airp_lastsucessfullyshipped).toDateString() : ''
+        }`;
       }
     } else {
       document.getElementById(`num${i + 1}`).innerText = '';
