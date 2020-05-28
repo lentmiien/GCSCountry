9ceee7e08000f0d3a7d7a644d9ff35b182b8ce1b
Lennart <lentmiien@gmail.com>
2020-05-28 15:18:03 +0900

    added all ship methods

--- public/javascripts/d3_controller.js
+++ public/javascripts/d3_controller.js
@@ -11,89 +11,56 @@ const margin = {
 const width = rect.width - margin.left - margin.right;
 const height = 300;
 
-// Create draw area EMS
-d3.select('#graph_area').append('h2').text('EMS');
-let svg = d3.select('#graph_area')
-  .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-    .attr('transform', 
-          `translate(${margin.left},${margin.top})`)
+function GenerateGraph(prefix, title) {
+  // Create draw area
+  d3.select('#graph_area').append('h2').text(title);
+  const svg = d3.select('#graph_area')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', 
+            `translate(${margin.left},${margin.top})`)
 
-// Prepare data
-let plotdata = data.map((entry, index) => {
-  return {
-    yval: entry.ems_days / (entry.ems_cnt > 0 ? entry.ems_cnt : 1),
-    xval: index,
-  }
-}).filter(entry => entry.yval > 0);
+  // Prepare data
+  let plotdata = data.map((entry, index) => {
+    return {
+      yval: entry[`${prefix}_days`] / (entry[`${prefix}_cnt`] > 0 ? entry[`${prefix}_cnt`] : 1),
+      xval: index,
+    }
+  }).filter(entry => entry.yval > 0);
 
-// X label
-let x = d3.scaleLinear()
-  .domain([0, 51])
-  .range([width, 0]);
-svg.append('g')
-  .attr('transform', `translate(0,${height})`)
-  .call(d3.axisBottom(x));
+  // X label
+  let x = d3.scaleLinear()
+    .domain([0, 51])
+    .range([width, 0]);
+  svg.append('g')
+    .attr('transform', `translate(0,${height})`)
+    .call(d3.axisBottom(x));
 
-// Y label
-let y = d3.scaleLinear()
-  .domain([0, d3.max(plotdata, d => d.yval)])
-  .range([height, 0]);
-svg.append('g')
-  .call(d3.axisLeft(y));
+  // Y label
+  let y = d3.scaleLinear()
+    .domain([0, d3.max(plotdata, d => d.yval)])
+    .range([height, 0]);
+  svg.append('g')
+    .call(d3.axisLeft(y));
 
-// Line
-svg
-  .append('path')
-  .datum(plotdata)
-  .attr('fill', 'none')
-  .attr('stroke', 'steelblue')
-  .attr('stroke-width', 3)
-  .attr('d', d3.line()
-    .x(d => x(d.xval))
-    .y(d => y(d.yval))
-  );
+  // Line
+  svg
+    .append('path')
+    .datum(plotdata)
+    .attr('fill', 'none')
+    .attr('stroke', 'steelblue')
+    .attr('stroke-width', 3)
+    .attr('d', d3.line()
+      .x(d => x(d.xval))
+      .y(d => y(d.yval))
+    );
+}
 
-// Create draw area Air Small Packet
-d3.select('#graph_area').append('h2').text('Air Small Packet');
-svg = d3.select('#graph_area')
-  .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-    .attr('transform', 
-          `translate(${margin.left},${margin.top})`)
-
-// Prepare data
-plotdata = data.map((entry, index) => {
-  return {
-    yval: entry.airsp_days / (entry.airsp_cnt > 0 ? entry.airsp_cnt : 1),
-    xval: index,
-  }
-}).filter(entry => entry.yval > 0);
-
-// X label
-svg.append('g')
-  .attr('transform', `translate(0,${height})`)
-  .call(d3.axisBottom(x));
-
-// Y label
-y = d3.scaleLinear()
-  .domain([0, d3.max(plotdata, d => d.yval)])
-  .range([height, 0]);
-svg.append('g')
-  .call(d3.axisLeft(y));
-
-// Line
-svg
-  .append('path')
-  .datum(plotdata)
-  .attr('fill', 'none')
-  .attr('stroke', 'steelblue')
-  .attr('stroke-width', 3)
-  .attr('d', d3.line()
-    .x(d => x(d.xval))
-    .y(d => y(d.yval))
-  );
+GenerateGraph('ems', 'EMS');
+GenerateGraph('ems', 'Air Small Packet');
+GenerateGraph('ems', 'SAL Registered');
+GenerateGraph('ems', 'SAL Parcel');
+GenerateGraph('ems', 'DHL');
+GenerateGraph('ems', 'Air Parcel');
